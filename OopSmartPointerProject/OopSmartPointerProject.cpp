c5beb25480e52466a26d073cb071d711f0ef28c1
maxefimov <efimov.top.academy@gmail.com>
2023-05-11 20:16:55 +0300

    11-05-23

--- OopSmartPointerProject/OopSmartPointerProject.cpp
+++ OopSmartPointerProject/OopSmartPointerProject.cpp
@@ -5,13 +5,18 @@ class SmartPointer
 {
     T* pointer;
 public:
-    static int count;
-
     SmartPointer(T* pointer = nullptr)
         : pointer{ pointer } {}
+
+    //SmartPointer(const SmartPointer& sp) = delete;
     SmartPointer(const SmartPointer& sp)
     {
-        pointer = sp.pointer;
+        pointer = new T;
+        *pointer = *sp.pointer;
+    }
+
+    SmartPointer(SmartPointer&& sp) noexcept : pointer{ sp.pointer }
+    {
         sp.pointer = nullptr;
     }
 
@@ -19,19 +24,38 @@ public:
     {
         delete pointer;
     }
+
     T& operator*() const { return *pointer; }
     T* operator->() const { return pointer; }
 
+    //SmartPointer& operator=(const SmartPointer& sp) = delete;
     SmartPointer& operator=(const SmartPointer& sp)
     {
         if (&sp == this)
             return *this;
 
         delete pointer;
-        pointer = sp.pointer;
-        sp.pointer = nullptr;
+        
+        pointer = new T;
+        *pointer = *sp.pointer;
+
         return *this;
     }
+
+    SmartPointer& operator=(SmartPointer&& sp) noexcept
+    {
+        if (&sp == this)
+            return *this;
+
+        delete pointer;
+
+        pointer = sp.pointer;
+        sp.pointer = nullptr;
+
+        return *this;
+    }
+
+    bool IsNull() const { return pointer == nullptr; }
 };
 
 
@@ -61,7 +85,7 @@ public:
 void Function()
 {
     //Resource* res = new Resource();
-    SmartPointer<Resource> sptr1(new Resource());
+    /*SmartPointer<Resource> sptr1(new Resource());
     sptr1->Value() = 100;
     SmartPointer<Resource> sptr2(new Resource());
     sptr2->Value() = 200;
@@ -74,7 +98,7 @@ void Function()
     SmartPointer<Resource> sptr4(sptr3);
     std::cout << sptr2.count << " " << sptr2->Value() << "\n";
     std::cout << sptr3.count << " " << sptr3->Value() << "\n";
-    std::cout << sptr4.count << " " << sptr4->Value() << "\n";
+    std::cout << sptr4.count << " " << sptr4->Value() << "\n";*/
     /*
     int x;
     std::cout << "input x: ";
@@ -89,7 +113,15 @@ void Function()
     //delete res;
 }
 
+SmartPointer<Resource> GenerateResource()
+{
+    SmartPointer<Resource> smPtr{ new Resource };
+    return smPtr;
+}
+
 int main()
 {
-    Function();
+    //Function();
+    SmartPointer<Resource> smPtr;
+    smPtr = GenerateResource();
 }
